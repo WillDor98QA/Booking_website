2690cdf5313bfaa0cc0e41e374249cc4861782ef
Ekow Yamoah <yamoahekow88@gmail.com>
2025-02-08 22:19:20 +0000

    signals

---
+++ bookings/signals.py
@@ -0,0 +1,14 @@
+import random
+from django.db.models.signals import pre_save
+from django.dispatch import receiver
+from .models import *
+
+
+@receiver(pre_save, sender=Appointments)
+def generate_clientID(sender, instance, **kwargs):
+    if not instance.clientID:
+        while True:
+            newID = str(random.randint(100000000, 999999999))  # Generate a 9-digit number
+            if not Appointments.objects.filter(clientID=newID).exists():
+                instance.clientID = newID
+                break
