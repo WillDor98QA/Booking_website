2544095b104c2b26431e404afed780fb5c2a51e5
WillDor98QA <dwilliametornam@gmail.com>
2025-03-13 03:33:19 +0000

    Adding changes to sidebar

--- bookingApp/settings.py
+++ bookingApp/settings.py
@@ -13,6 +13,11 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 from pathlib import Path
 import os
 
+import pymysql
+pymysql.version_info = (1, 4, 3, "final", 0)
+pymysql.install_as_MySQLdb()
+
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -80,21 +85,16 @@ WSGI_APPLICATION = 'bookingApp.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
+
 
 # MySQL Server
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'crown_kreation',
-        'USER': 'yme',
-        'PASSWORD': 'Password@123',
-        'HOST':'localhost',
+     'default': {
+         'ENGINE': 'django.db.backends.mysql',
+         'NAME': 'crown_kreation',
+         'USER': 'root',
+         'PASSWORD': 'Password@123',
+         'HOST':'localhost',
         'PORT':'3306',
     }
 }
